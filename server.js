27c450ba2af23c28832ac1aed4d86882d0af0960
Raymond <20248577+javaarchive@users.noreply.github.com>
2021-07-24 14:07:28 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const app = express()
- 
+const dns2 = require('dns2');
+const dns = new dns2({dns:"1.1.1.1"});
 app.get('/', function (req, res) {
   res.send('Hi')
-})
+});
+
+app.get('/resolve', async (req, res) => {
+  res.json(await dns.resolve(req.query.host || "example.com"));
+});
+
+app.get('/resolveA', async (req, res) => {
+  res.json(await dns.resolveA(req.query.host || "example.com"));
+});
  
 app.listen(process.env.PORT)
